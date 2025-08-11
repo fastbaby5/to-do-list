9d9f58968593e0c83ec5ea3431a9052b7be2cf60
Baha salameh <bahaasalamehh@gmail.com>
2025-08-11 03:35:32 +0300

    add Mark feature

--- program.py
+++ program.py
@@ -1,9 +1,10 @@
-# sdhfasdfsdf
+from colorama import init, Fore, Style
 import csv
 import os
 
 List=[]
 FILENAME = "todoList.csv"
+init(autoreset=True) 
 
 
 #========================================================================
@@ -12,69 +13,100 @@ FILENAME = "todoList.csv"
 
 
 def SHOW():
-    with open(FILENAME, mode='r', newline='') as file: 
-        todo = list(csv.reader(file))
+    with open(FILENAME, mode='r', newline='', encoding='utf-8') as file: 
+        todo = list(csv.DictReader(file))
+
         if not todo:
-            print("Empty list\n")
+            print(Fore.CYAN+"Empty list\n")
         else:
             for i,x in enumerate(todo, 1):
-                print(f"{i}.{x[0]}")
+                status_color = Fore.GREEN if x['Status'].lower() == "done" else Fore.YELLOW + Style.NORMAL
+                print(f"{Fore.LIGHTBLUE_EX}{i}.{Style.RESET_ALL} {x['Task']},\t\t\t Status: {status_color}{x['Status']}{Style.RESET_ALL}")
+    
 
-def ADD(thing):
+def ADD(thing, status):
+    fieldnames = ["Task", "Status"]
     with open(FILENAME, mode='a', newline='') as file:
-        csv.writer(file).writerow([thing])
+        csv.DictWriter(file, fieldnames=fieldnames).writerow({"Task":thing , "Status":status})
         return True
     
+
 def DELETE(number):
      with open(FILENAME, mode='r', newline='') as file: 
         todo = list(csv.reader(file))
         if 1 <= number <= len(todo):
             try:
-                removed = todo.pop(number - 1)
+                removed = todo.pop(number )
                 print(f"deleted {removed[0]}")
                 with open(FILENAME, mode='w', newline='') as file:
                     csv.writer(file).writerows(todo)
             except:
-                print("invalid input\n")
-    
+                print(Fore.RED+"could not delete it\n")
+        else:
+            print(Fore.RED+"invalid input\n")
 
+def change_status(number, new_status):
+    with open(FILENAME, mode='r', newline='', encoding="utf-8") as file:
+        todo = list(csv.reader(file))
+
+        if 1 <= number <= len(todo):
+            try:
+                todo[number][1] = new_status
+                print(f"Updated '{todo[number][0]}' to status '{new_status}'")
+
+                with open(FILENAME, mode='w', newline='', encoding="utf-8") as file:
+                    csv.writer(file).writerows(todo)
+
+            except Exception as e:
+                print(Fore.RED+"Could not update it\n", e)
+        else:
+            print(Fore.RED+"Invalid input\n")
 #========================================================================
 
 
 
 # Check if file exists, if not create an empty one
 if not os.path.exists(FILENAME):
-    print("creating file csv")
+    print(f"\n{Fore.BLUE} creating file csv")
     with open(FILENAME, mode='w', newline='') as file:
-        pass  # creates an empty file
+        fieldnames = ["Task", "Status"]
+        csv.DictWriter(file , fieldnames=fieldnames).writeheader()
 
 
 #========================================================================
 
-
-print("\nWELCOME TO TODO LIST PROGRAM\n")
-print("1. Show list\n2. Add to list\n3. Delete from list\n0. Exit")
+print(f"{Fore.LIGHTBLUE_EX + Style.BRIGHT}\nWELCOME TO TODO LIST PROGRAM\n {Style.RESET_ALL}")
+print(f"{Fore.LIGHTBLUE_EX}1.{Style.RESET_ALL} Show list\n{Fore.LIGHTBLUE_EX}2. {Style.RESET_ALL}Add to list\n{Fore.LIGHTBLUE_EX}3. {Style.RESET_ALL}Delete from list\n{Fore.LIGHTBLUE_EX}4. {Style.RESET_ALL}Mark Done\n{Fore.LIGHTBLUE_EX}0. {Style.RESET_ALL}Exit")
 
 while True:    
     try:
-        print("***********************************************")
-        x = int(input("\nChoose one: "))
+        print(Fore.LIGHTBLUE_EX+"***********************************************")
+        x = int(input(Fore.GREEN + Style.BRIGHT + "\nChoose one: "))
     except ValueError:
-        print("Please enter a valid number (1–4).\n")
+        print(Fore.RED+"Please enter a valid number (1–4).\n")
         continue
     if x == 1:
+        print("\n")
         SHOW()
+        with open(FILENAME, "r", newline="", encoding="utf-8") as file:
+            reader = csv.reader(file)
+            row_count = sum(1 for _ in reader ) -1
+            print(f"{Fore.CYAN + Style.BRIGHT}\nNumber of todo:{Style.RESET_ALL}", row_count)
 
     elif x == 2:
-        add = input("enter what you want to add: ")
-        ADD(add)
+        add = input(Fore.GREEN + Style.BRIGHT +f"enter what you want to add:{Style.RESET_ALL} ")
+        ADD(add, "in progress")
 
     elif x == 3:
         SHOW()
-        y=int(input("chose the one you want to delete\n"))
+        y=int(input(Fore.GREEN + Style.BRIGHT +f"\nchose the one you want to delete:{Style.RESET_ALL}"))
         DELETE(y)
+    elif x == 4:
+        SHOW()
+        G = int(input(Fore.GREEN + Style.BRIGHT +f"\nchose the one you want to mark as Done:{Style.RESET_ALL}"))
+        change_status(G , 'Done')
     elif x == 0:
-        print("Goodbye!\n")
+        print(f"{Style.RESET_ALL}Goodbye!\n")
         break
     else:
-        print("invalied input\n")
+        print(Fore.RED +"invalied input\n")
